8109b4bf6442c882c76197cec4b0452e3e101ade
seanlee726 <wansunlee@gmail.com>
2018-09-15 02:35:30 +0000

    dojo => views (handle empty element)

--- dojo/views.py
+++ dojo/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 
 def mysum(request, numbers):
-    result = sum(map(int, numbers.split("/")))
+    result = sum(map(lambda s: int(s or 0), numbers.split("/")))
     return HttpResponse(result)
 
 

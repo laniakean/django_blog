0e09eebe9b213f073fc74751f0abd00f74748a72
seanlee726 <wansunlee@gmail.com>
2018-09-16 07:29:13 +0000

    blog => models(add params and input validator)

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,16 @@
 # blog/models.py
 # 모델의 역할은 DB에서 데이터를 가져오는 역할
 # 모든 클래스는 단수형으로 쓴다!
-
+import re
 from django.db import models
+from django.forms import ValidationError
+
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid lnglat Type')
+
+
+
 
 class Post(models.Model):
     title = models.CharField(max_length = 100, 
@@ -18,5 +26,16 @@ class Post(models.Model):
     content = models.TextField(verbose_name = '내용')               #글자의 길이 제한이 없는 문자열! (길이무제한 : TextField 사용)
     created_at = models.DateTimeField(auto_now_add=True) #auto_now_add : 최초 생성일을 입력하는 방식(일시)
     updated_at = models.DateTimeField(auto_now=True)     #auto_now : 파일이 새로 저장된 일자를 입력하는 방식(일시)
+
+    tags = models.CharField(max_length = 100, blank=True)
+    lnglat = models.CharField(max_length = 50,
+        validators = [lnglat_validator],
+        help_text = "경도와 위도 포멧으로 입력해 주세요.",
+        blank = True    
+    )
+
+
+
+
 # Create your models here.
 
